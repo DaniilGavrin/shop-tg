e95b167560b1bb4601a2a6ab6b80a79df701bc77
Daniil <bksbank@bk.ru>
2026-05-23 09:50:53 +0300

    Fix Telegrama SDK RU Blocked safe loading 2

--- app/layout.tsx
+++ app/layout.tsx
@@ -44,7 +44,7 @@ export default function RootLayout({
       >
         {/* Telegram SDK НЕ критичен */}
         <Script
-          src="https://telegram.org/js/telegram-web-app.js"
+          src="/telegram-web-app.js"
           strategy="afterInteractive"
         />
 
