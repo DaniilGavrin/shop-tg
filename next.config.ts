5ff030f85471947a6bedf2effba75cdb3dcf62c6
Daniil <bksbank@bk.ru>
2026-06-12 18:32:15 +0300

    ЕЩЁ БОЛЬШЕ КЭША

--- next.config.ts
+++ next.config.ts
@@ -3,17 +3,22 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   devIndicators: false,
   
-  // Добавляем кастомные заголовки
   async headers() {
     return [
       {
-        // Ловим все файлы с расширениями шрифтов в папке public
-        source: "/:path*.(woff|woff2|ttf|eot|otf)",
+        source: "/fonts/:path*",
         headers: [
           {
             key: "Cache-Control",
-            // 31536000 секунд = 1 год. 
-            // immutable говорит браузеру: "Даже не проверяй, изменился ли файл, он вечный"
+            value: "public, max-age=31536000, immutable",
+          },
+        ],
+      },
+      {
+        source: "/telegram-web-app.js",
+        headers: [
+          {
+            key: "Cache-Control",
             value: "public, max-age=31536000, immutable",
           },
         ],
