54c20f9656f3b41ccb9c50ebea6914a93fd8babb
Daniil <bksbank@bk.ru>
2026-06-15 11:12:42 +0300

    Блять

--- app/components/YandexMetrica.tsx
+++ app/components/YandexMetrica.tsx
@@ -9,26 +9,34 @@ export function YandexMetrika() {
     <>
       <Script
         id="yandex-metrika"
-        strategy="afterInteractive" // Загружается ПОСЛЕ того, как страница стала интерактивной (не блокирует LCP)
+        strategy="afterInteractive"
         dangerouslySetInnerHTML={{
           __html: `
             (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
             m[i].l=1*new Date();
             for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
             k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
-            })(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+            })(window, document, "script", "https://mc.yandex.ru/metrika/tag.js?id=${YANDEX_METRIKA_ID}", "ym");
 
             ym(${YANDEX_METRIKA_ID}, "init", {
-              ssr: true,
-              webvisor: true,
               clickmap: true,
-              ecommerce: "dataLayer",
+              trackLinks: true,
               accurateTrackBounce: true,
-              trackLinks: true
+              trackHash: true,
+              webvisor: true
             });
           `,
         }}
       />
+      <noscript>
+        <div>
+          <img 
+            src={`https://mc.yandex.ru/watch/${YANDEX_METRIKA_ID}`} 
+            style={{ position: 'absolute', left: '-9999px' }} 
+            alt="" 
+          />
+        </div>
+      </noscript>
     </>
   );
 }
